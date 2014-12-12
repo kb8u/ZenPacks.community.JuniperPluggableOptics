12a7e8b06559ee69c8f3ed82e47fa722c44c1d0c
Russell Dwarshuis <rjd@merit.edu>
2014-12-12 18:01:54 -0500

    Fixed class on the other rrdtemplate

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.JuniperPluggableOptics"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 AUTHOR = "Russell Dwarshuis"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
